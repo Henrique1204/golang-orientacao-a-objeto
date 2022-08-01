e977555a19ea674415007c22b5054f79758e1873
Paulo <pauloh16.pdr@gmail.com>
2022-08-01 19:08:50 -0300

    Criar primeiro exemplo de struct

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
+func main() {
+	primeiraConta := ContaCorrente{titular: "Paulo", numeroAgencia: 589, numeroConta: 123456, saldo: 323.45}
+
+	fmt.Println(primeiraConta)
+}
