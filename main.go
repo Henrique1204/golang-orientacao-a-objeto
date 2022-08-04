51abacd0f7fa6fdf24e2483b0c70eca45bc2ea27
Paulo <pauloh16.pdr@gmail.com>
2022-08-03 22:08:45 -0300

    Criando metodo de sacar

--- main.go
+++ main.go
@@ -9,6 +9,20 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	const MINIMO_SAQUE = 1
+
+	podeSacar := valorDoSaque >= MINIMO_SAQUE && valorDoSaque <= c.saldo
+
+	if podeSacar {
+		c.saldo -= valorDoSaque
+
+		return "Saque ocorreu com sucesso!"
+	}
+
+	return "Erro ao sacar!"
+}
+
 func main() {
 	primeiraConta := ContaCorrente{titular: "Paulo", numeroAgencia: 589, numeroConta: 123456, saldo: 323.45}
 
@@ -18,4 +32,7 @@ func main() {
 	segundaConta = new(ContaCorrente)
 
 	fmt.Println(segundaConta)
+
+	fmt.Println(primeiraConta.Sacar(200))
+	fmt.Println(primeiraConta.Sacar(200))
 }
