f14631f7ac0b8d50637910f3513396374d8b58f6
Paulo <pauloh16.pdr@gmail.com>
2022-08-06 20:05:29 -0300

    Criar metodo para depositar

--- main.go
+++ main.go
@@ -9,18 +9,30 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	const MINIMO_SAQUE = 1
+func (c *ContaCorrente) Sacar(valorDoSaque float64) (string, float64) {
+	const SAQUE_MINIMO = 0
 
-	podeSacar := valorDoSaque >= MINIMO_SAQUE && valorDoSaque <= c.saldo
+	podeSacar := valorDoSaque > SAQUE_MINIMO && valorDoSaque <= c.saldo
 
 	if podeSacar {
 		c.saldo -= valorDoSaque
 
-		return "Saque ocorreu com sucesso!"
+		return "Saque ocorreu com sucesso!", c.saldo
 	}
 
-	return "Erro ao sacar!"
+	return "Erro ao sacar!", c.saldo
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	const MINIMO_DEPOSITO = 0
+
+	if valorDoDeposito > MINIMO_DEPOSITO {
+		c.saldo += valorDoDeposito
+
+		return "Deposito ocorreu com sucesso!", c.saldo
+	}
+
+	return "Erro ao depositar!", c.saldo
 }
 
 func main() {
@@ -35,4 +47,7 @@ func main() {
 
 	fmt.Println(primeiraConta.Sacar(200))
 	fmt.Println(primeiraConta.Sacar(200))
+	fmt.Println(primeiraConta.Depositar(25.45))
+
+	fmt.Println(primeiraConta)
 }
